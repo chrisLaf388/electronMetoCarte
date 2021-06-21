cf125e760923c40e5f2c9ccbd23142a169b20063
chris <christophe.lafenetre@gmail.com>
2021-06-21 11:21:26 +0200

    ok carte

--- src/assets/js/weather.js
+++ src/assets/js/weather.js
@@ -2,7 +2,7 @@ async function weatherEtMaps(ville){
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${ville}&appid=1ce64129da731d692308f766613a1037`;
     const response = await fetch(url);
     const weather = await response.json();
-    temperatureDiv.innerHTML = Math.round(weather.main.temp - 273.15);
+    temperatureDiv.innerHTML = "La température est de : "+Math.round(weather.main.temp - 273.15);
     //latitude et longitude
     latitude = weather.coord.lat;
     longitude = weather.coord.lon;
